caf0cb47ab9fa56ae2f3eab0dc10d45c9e72f223
Moomins07 <davidrsmith04@gmail.com>
2024-02-26 17:35:29 +0000

    changed repository for revised version of project

--- js/script.js
+++ js/script.js
@@ -1,67 +1,123 @@
 // Set game board module, loop through array and display on DOM
 
-const ticTacToe = (function () {
-  const X_CLASS = 'x';
-  const CIRCLE_CLASS = 'circle';
-  let cellElements;
-  const overlay = document.querySelector('.overlay');
 
-  const WIN_CONDITIONS = [
-    [0, 1, 2], // Top row
-    [3, 4, 5], // Middle row
-    [6, 7, 8], // Bottom row
+// GAMEBOARD FACTORY FUNCTION
 
-    [0, 3, 6], // Left column
-    [1, 4, 7], // Middle column
-    [2, 5, 8], // Right column
+const GameBoard = (() => {
 
-    [0, 4, 8], // Diagonal from top-left to bottom-right
-    [2, 4, 6], // Diagonal from top-right to bottom-left
-  ];
+  const Config = {
+    X_CLASS: 'x',
+    CIRCLE_CLASS: 'circle',
+    circleTurn: undefined,
+    WIN_CONDITIONS: [
+      [0, 1, 2], // Top row
+      [3, 4, 5], // Middle row
+      [6, 7, 8], // Bottom row
 
-  const gameBoard = Array(9).fill('');
-  const boardEl = document.getElementById('board');
+      [0, 3, 6], // Left column
+      [1, 4, 7], // Middle column
+      [2, 5, 8], // Right column
+
+      [0, 4, 8], // Diagonal from top-left to bottom-right
+      [2, 4, 6], // Diagonal from top-right to bottom-left
+    ]
+  };
+
+  const board = Array(9).fill('');
   const winningMessageTextElement = document.getElementById('winning-text');
-  let passedCheck = null;
-  let circleTurn;
+
+
+  function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+  }
+
+  function endGame(draw) {
+    if (draw) {
+      winningMessageTextElement.innerText = 'Draw!';
+    } else {
+      winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"
+        } Win!`;
+    }
+    overlay.classList.remove('d-none');
+    overlay.classList.add('d-flex');
+  }
+
+  function changeTurn() {
+    Config.circleTurn = !Config.circleTurn;
+    console.log(Config.circleTurn)
+    GameUI.playerTurnBorders();
+  }
+
+  // CHECK FOR DRAW
+
+  // CHECK FOR WIN
+
+  // CHECK FOR END GAME
+
+  return {
+
+    getBoard() {
+      return [...board]
+    },
+
+    handleClick(cell) {
+      const CIRCLE_CLASS = Config.CIRCLE_CLASS
+      const X_CLASS = Config.X_CLASS
+
+      const currentClass = Config.circleTurn ? CIRCLE_CLASS : X_CLASS;
+      console.log(currentClass)
+      // GUARD CLAUSE TO PREVENT CLICKING TWICE ON A CELL
+      if (
+        cell.classList.contains(X_CLASS) ||
+        cell.classList.contains(CIRCLE_CLASS)
+      ) {
+        // Cell is already occupied; do nothing.
+        return;
+      }
+
+      placeMark(cell, currentClass);
+      changeTurn()
+      // if (checkWin(currentClass)) {
+      //   endGame(false);
+      // } else if (isDraw()) {
+      //   endGame(true);
+      // } else changeTurn();
+    },
+
+    createUser(name) {
+      return { name }
+    },
+
+    resetBoard() {
+      cellElements.forEach((cell) => {
+        cell.classList.remove(Config.X_CLASS);
+        cell.classList.remove(Config.CIRCLE_CLASS);
+      });
+
+      GameUI.restartGame()
+    },
+
+
+  }
+
+})();
+
+
+
+const GameUI = (() => {
+  const overlay = document.querySelector('.overlay');
+  const boardEl = document.getElementById('board');
+  let usersCreated = null;
   const playersEl = document.getElementById('playersEl');
   const playerTurnBox1 = playersEl.children[1];
   const playerTurnBox2 = playersEl.children[2];
+  const cellElements = document.querySelectorAll('[data-cell]');
+  const backBtnEl = document.getElementById('backBtn');
 
-  let setGameBoard = () => {
-    const backBtnEl = document.getElementById('backBtn');
 
-    boardEl.classList.add(X_CLASS);
-    utils.addRemoveClass(playersEl, 'remove', 'd-none');
-    // playersEl.classList.remove('d-none');
-    utils.addRemoveClass(backBtnEl, 'remove', 'd-none');
-    // backBtnEl.classList.remove('d-none');
-    utils.addRemoveClass(backBtnEl, 'add', 'd-flex');
-    // backBtnEl.classList.add('d-flex');
-    hideUsers(true);
-    displayGameBoard(true);
+  // PRIVATE FUNCTIONS
 
-    boardEl.innerHTML = '';
 
-    gameBoard.forEach((cell) => {
-      let div = document.createElement('div');
-      div.classList.add('cell');
-      div.dataset.cell = '';
-      boardEl.appendChild(div);
-    });
-    cellElements = document.querySelectorAll('[data-cell]');
-
-    createPlayers();
-  };
-
-  function displayGameBoard(show) {
-    if (show) {
-      boardEl.classList.remove('d-none');
-    } else {
-      boardEl.classList.add('d-none');
-      hideUsers(false);
-    }
-  }
 
   function hideUsers(hide) {
     const mainContainerEl = document.getElementById('mainContainerEl');
@@ -74,182 +130,123 @@ const ticTacToe = (function () {
     }
   }
 
-  function createPlayers() {
-    const playerInput = document.querySelectorAll('.playerInput');
-
-    if (
-      playerInput[0].value.trim() === '' ||
-      playerInput[1].value.trim() === ''
-    ) {
-      passedCheck = false;
-      alert('Please input a name.');
+  function displayGameBoard(show) {
+    if (show) {
+      boardEl.classList.remove('d-none');
     } else {
-      // Targets first
-      playersEl.children[1].firstElementChild.textContent =
-        utils.capitalizeFirstLetter(playerInput[0].value);
-      playersEl.children[2].firstElementChild.textContent =
-        utils.capitalizeFirstLetter(playerInput[1].value);
-      passedCheck = true;
+      boardEl.classList.add('d-none');
+      hideUsers(false);
     }
   }
 
-  function init() {
-    setUpListeners();
-  }
-
-  function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass);
-  }
-
-  function handleClick(cell) {
-    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
-
-    if (
-      cell.classList.contains(X_CLASS) ||
-      cell.classList.contains(CIRCLE_CLASS)
-    ) {
-      // Cell is already occupied; do nothing.
-      return;
-    }
-    // else if (
-    //   boardEl.classList.contains(X_CLASS) &&
-    //   cell.dataset.cell !== true
-    // ) {
-    //   cell.dataset.cell = true;
-    //   cell.classList.add(X_CLASS);
-    // } else if (
-    //   boardEl.classList.contains(CIRCLE_CLASS) &&
-    //   cell.dataset.cell !== true
-    // ) {
-    //   cell.dataset.cell = true;
-    //   cell.classList.add(CIRCLE_CLASS);
-    // }
-    placeMark(cell, currentClass);
-    if (checkWin(currentClass)) {
-      endGame(false);
-    } else if (isDraw()) {
-      endGame(true);
-    } else changeTurn();
-  }
-
-  function isDraw() {
-    return [...cellElements].every((cell) => {
-      return (
-        cell.classList.contains(X_CLASS) ||
-        cell.classList.contains(CIRCLE_CLASS)
-      );
-    });
-  }
-
-  function checkWin(currentClass) {
-    return WIN_CONDITIONS.some((combinations) => {
-      return combinations.every((index) => {
-        return cellElements[index].classList.contains(currentClass);
-      });
-    });
-  }
-
-  function endGame(draw) {
-    if (draw) {
-      winningMessageTextElement.innerText = 'Draw!';
-    } else {
-      winningMessageTextElement.innerText = `${
-        circleTurn ? "O's" : "X's"
-      } Win!`;
-    }
-    overlay.classList.remove('d-none');
-    overlay.classList.add('d-flex');
-  }
-
-  function changeTurn() {
-    // If board contains x, remove x and add circle, if circle, remove circle and add x. 2 toggles? Definitely more efficient ways of doing this but it works
-
-    // if (boardEl.classList.contains(X_CLASS)) {
-    //   boardEl.classList.remove(X_CLASS);
-    //   boardEl.classList.add(CIRCLE_CLASS);
-    // } else if (boardEl.classList.contains(CIRCLE_CLASS)) {
-    //   boardEl.classList.remove(CIRCLE_CLASS);
-    //   boardEl.classList.add(X_CLASS);
-    // }
-
-    circleTurn = !circleTurn;
-    checkPlayerTurn();
-  }
-
-  function checkPlayerTurn() {
-    if (circleTurn) {
-      playerTurnBox1.classList.add('border-active');
-      playerTurnBox2.classList.remove('border-active');
-      // playerTurnBox1.style.border = '0.25rem solid black';
-      // playerTurnBox1.style.borderRadius = '10px';
-      // playerTurnBox2.style.border = '';
-    } else {
-      playerTurnBox2.classList.add('border-active');
-      playerTurnBox1.classList.remove('border-active');
-      // playerTurnBox2.style.border = '0.25rem solid black';
-      // playerTurnBox2.style.borderRadius = '10px';
-      // playerTurnBox1.style.border = '';
-    }
-  }
-
-  function restartGame() {
-    overlay.classList.toggle('d-none');
-    overlay.classList.toggle('d-flex');
-
-    cellElements.forEach((cell) => {
-      cell.classList.remove(X_CLASS);
-      cell.classList.remove(CIRCLE_CLASS);
-    });
-
-    playerTurnBox1.classList.remove('border-active');
-    playerTurnBox2.classList.remove('border-active');
-  }
-
-  function setUpListeners() {
-    const startBtn = document.getElementById('start');
-    const restartBtn = document.getElementById('restart');
-    const backBtn = document.getElementById('backBtn');
-
-    backBtn.addEventListener('click', () => {
-      displayGameBoard(false);
-      utils.addRemoveClass(playersEl, 'add', 'd-none');
-      utils.addRemoveClass(backBtn, 'add', 'd-none');
-    });
-
-    startBtn.addEventListener('click', (e) => {
-      e.preventDefault();
-      createPlayers();
-      if (passedCheck) {
-        setGameBoard();
-      }
-      playerTurnBox2.classList.remove('border-active');
-      playerTurnBox1.classList.remove('border-active');
-    });
-
-    restartBtn.addEventListener('click', restartGame);
-
-    // BOARD CLICKS EVENT DELEGATION
-    boardEl.addEventListener('click', (e) => {
-      if (e.target.classList.contains('cell')) {
-        handleClick(e.target);
-      }
-    });
-  }
-
+  // ONLY RETURN PUBLIC FUNCCTIONS
   return {
-    init,
-  };
-})();
 
-// Factory function to create players using input?
+    // I return this object because I use it on line 46 inside GameBoard module
+    restartGame() {
+      overlay.classList.toggle('d-none');
+      overlay.classList.toggle('d-flex');
+      playerTurnBox1.classList.remove('border-active');
+      playerTurnBox2.classList.remove('border-active');
+    },
 
-// On START click, remove elements except for gameBoard.
+    // Required in GameBoard module
+    createPlayers() {
+      const playerInput = document.querySelectorAll('.playerInput');
 
-// Check which player's turn it is, place marker by adding .x or .circle to a specific cell.
+      if (
+        playerInput[0].value.trim() === '' ||
+        playerInput[1].value.trim() === ''
+      ) {
+        usersCreated = false;
+        alert('Please fill both fields.');
+      } else {
 
-// Check win conditions every click, if no win con, draw.
+        const player1 = GameBoard.createUser(playerInput[0].value)
+        const player2 = GameBoard.createUser(playerInput[1].value)
 
-const utils = (function utilities() {
+        // Targets first
+        playersEl.children[1].firstElementChild.textContent = utils.capitalizeFirstLetter(player1.name)
+        playersEl.children[2].firstElementChild.textContent = utils.capitalizeFirstLetter(player2.name)
+        usersCreated = true;
+      }
+    },
+
+    // Required in GameBoard module
+    setGameBoard() {
+      boardEl.classList.add(GameBoard.X_CLASS);
+      utils.addRemoveClass(playersEl, 'remove', 'd-none');
+      utils.addRemoveClass(backBtnEl, 'remove', 'd-none');
+      utils.addRemoveClass(backBtnEl, 'add', 'd-flex');
+      hideUsers(true);
+      displayGameBoard(true);
+
+      boardEl.innerHTML = '';
+
+      const board = GameBoard.getBoard()
+
+      board.forEach((cell) => {
+        let div = document.createElement('div');
+        div.classList.add('cell');
+        div.dataset.cell = '';
+        boardEl.appendChild(div);
+      });
+
+      GameUI.createPlayers();
+    },
+
+    // Required in GameBoard module
+    playerTurnBorders() {
+      if (circleTurn) {
+        playerTurnBox1.classList.add('border-active');
+        playerTurnBox2.classList.remove('border-active');
+      } else {
+        playerTurnBox2.classList.add('border-active');
+        playerTurnBox1.classList.remove('border-active');
+      }
+    },
+
+    // Invoked in DOMContentLoaded listener function
+    setUpListeners() {
+      const startBtn = document.getElementById('start');
+      const restartBtn = document.getElementById('restart');
+      const backBtn = document.getElementById('backBtn');
+
+      backBtn.addEventListener('click', () => {
+        displayGameBoard(false);
+        utils.addRemoveClass(playersEl, 'add', 'd-none');
+        utils.addRemoveClass(backBtn, 'add', 'd-none');
+      });
+
+      startBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        GameUI.createPlayers();
+        if (usersCreated) {
+          GameUI.setGameBoard();
+        }
+        playerTurnBox2.classList.remove('border-active');
+        playerTurnBox1.classList.remove('border-active');
+      });
+
+      restartBtn.addEventListener('click', GameBoard.resetBoard);
+
+      // BOARD CLICKS EVENT DELEGATION
+      boardEl.addEventListener('click', (e) => {
+        if (e.target.classList.contains('cell')) {
+          GameBoard.handleClick(e.target);
+        }
+      });
+    },
+
+  }
+
+})()
+
+
+
+
+
+const utils = (function () {
   function capitalizeFirstLetter(str) {
     if (!str || typeof str !== 'string') {
       return '';
@@ -269,5 +266,5 @@ const utils = (function utilities() {
 })();
 
 document.addEventListener('DOMContentLoaded', function () {
-  ticTacToe.init();
+  GameUI.setUpListeners();
 });
